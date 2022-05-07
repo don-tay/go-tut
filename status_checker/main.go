3eb25e1f9b53794228e5ab82d6575a27cdb23953
Don <dontay0209@gmail.com>
2022-05-07 11:29:22 +0800

    Add status checker script

---
+++ status_checker/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://amazon.com",
+		"http://stackoverflow.com",
+		"http://twitter.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	var msg string
+	if err != nil {
+		msg = link + " might be down"
+	} else {
+		msg = link + " is up"
+	}
+	c <- link
+	fmt.Println(msg)
+}
